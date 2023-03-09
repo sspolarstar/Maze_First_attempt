6a867058d6b40106b5cd14f83588d86c1e37f40c
sspolarstar <47253708+sspolarstar@users.noreply.github.com>
2023-03-09 10:18:58 -0700

    Update boulder.cpp

--- boulder.cpp
+++ boulder.cpp
@@ -1,18 +1,18 @@
 #include "boulder.hpp"
 
-boulder(position, ) {
-
-}
-
-void Update() {
-
-}
-
-
-
-void Draw() {
-
-}
-void push(char direction, float speed) {
-
-}
+//boulder(position, ) {
+//
+//}
+//
+//void Update() {
+//
+//}
+//
+//
+//
+//void Draw() {
+//
+//}
+//void push(char direction, float speed) {
+//
+//}
